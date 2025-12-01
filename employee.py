44c2bb7f39f289fe0dd808b9d9db0e23810b25b8
harshitamachakanur-blip <harshitamachakanur@gmail.com>
2025-12-01 15:12:08 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,7 +1,7 @@
 def employee_info(name, emp_id, department, salary):
     return f"Employee Name: {name}\nEmployee ID: {emp_id}\nDepartment: {department}\nSalary: ₹{salary}"
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     name = "Rahul"
     emp_id = "EMP1024"
     department = "IT"
